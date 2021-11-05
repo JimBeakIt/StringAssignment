e4f08a7bc13bfb3c0bcd7651c21edaab32b67a08
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2021-11-05 10:36:31 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName1 = "James";
+            string userName2 = "Rumey";
+            string userName3 = "Larry";
+            string dateString = DateTime.Today.ToShortDateString();
+
+            userName2 = userName2.ToUpper();
+
+            string str = "Hello " + userName1 + ", " + userName2 + " and " + userName3  + ". Today is " + dateString + ".";
+            System.Console.WriteLine(str);
+            
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("")
+        }
+    }
+}
